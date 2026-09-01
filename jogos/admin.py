1bc7c1f7ad1d1bb073e1e39ce7a01ba4b5fadb7d
KaykezDev <kaiquedevv@gmail.com>
2026-09-01 20:17:21 -0300

    Models, admin e cadastro de jogos teste

--- jogos/admin.py
+++ jogos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Categoria, Plataforma, Jogo
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Plataforma)
+class PlataformaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Jogo)
+class JogoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'tipo_midia', 'estoque')
+    search_fields = ('nome', 'categorias__nome', 'plataformas__nome')
+    filter_horizontal = ('categorias', 'plataformas')
+    list_filter = ('tipo_midia', 'categorias', 'plataformas')
