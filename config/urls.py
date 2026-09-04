625d118273f11f9e47b4f610cad794fbc6622b63
KaykezDev <kaiquedevv@gmail.com>
2026-09-04 00:17:31 -0300

    CORREÇÃO DEFINITIVA DAS PAGINAS HTML

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app.urls')),
     path('jogos/', include('jogos.urls')),
     path('contas/', include('contas.urls')),
     path('app/', include('app.urls')),
