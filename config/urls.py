31fa60ca895812e746537dcf287455f2443ec989
MakxuelDev <matheuszazum1@gmail.com>
2026-09-10 20:58:20 -0300

    Fix URL include for config.urls in urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('', include('config.urls')),
     path('jogos/', include('jogos.urls')),
     path('contas/', include('contas.urls')),
     path('config/', include('config.urls')),
